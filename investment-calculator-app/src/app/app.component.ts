f9bcdc9c540d539294b174a13b530b0e0aab62ea
Polgs10 <hpaulgarciasim@gmail.com>
2025-06-18 13:03:40 +0200

    Version 1.2

---
+++ investment-calculator-app/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component, signal } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { UserInputComponent } from './user-input/user-input.component';
+import { InvestmentInput } from './investment-input.model';
+import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'investment-calculator-app';
+
+}
